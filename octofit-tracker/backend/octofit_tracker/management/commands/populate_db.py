27255a3f48fb0f4a07a0af5a3799154c2bdabb4f
Roza Jiang <rozajay@github.com>
2026-03-04 23:33:30 +0000

    Add initial Django app structure with models, views, serializers, and settings

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,44 @@
+from django.core.management.base import BaseCommand
+from octofit_tracker.models import User, Team, Activity, Workout, Leaderboard
+from datetime import date
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **options):
+        # Clear existing data
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Workout.objects.all().delete()
+        Leaderboard.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        users = [
+            User(name='Spider-Man', email='spiderman@marvel.com', team=marvel),
+            User(name='Iron Man', email='ironman@marvel.com', team=marvel),
+            User(name='Wonder Woman', email='wonderwoman@dc.com', team=dc),
+            User(name='Batman', email='batman@dc.com', team=dc),
+        ]
+        for user in users:
+            user.save()
+
+        # Create activities
+        Activity.objects.create(user=users[0], type='Running', duration=30, calories=300, date=date.today())
+        Activity.objects.create(user=users[1], type='Cycling', duration=45, calories=450, date=date.today())
+        Activity.objects.create(user=users[2], type='Swimming', duration=60, calories=600, date=date.today())
+        Activity.objects.create(user=users[3], type='Yoga', duration=40, calories=200, date=date.today())
+
+        # Create workouts
+        Workout.objects.create(name='Hero HIIT', description='High intensity interval training for heroes.', suggested_for='Marvel')
+        Workout.objects.create(name='Power Yoga', description='Yoga for strength and flexibility.', suggested_for='DC')
+
+        # Create leaderboard
+        Leaderboard.objects.create(team=marvel, points=750)
+        Leaderboard.objects.create(team=dc, points=800)
+
+        self.stdout.write(self.style.SUCCESS('octofit_db database populated with test data.'))
